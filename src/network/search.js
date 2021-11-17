1ea827c87d465db029e5837b22b1ceeab0c5486d
DurainBubbles <1791229116@qq.com>
2021-11-17 22:21:14 +0800

    add getHotSearch

--- src/network/search.js
+++ src/network/search.js
@@ -10,3 +10,16 @@ export const getSearch = (keywords,limit=30,offset=0,type=1) => request({
         type
     }
 })
+
+// 获取正在输入时推荐单曲与歌单
+export const getSuggest = keywords => request({
+    url:'/search/suggest',
+    params:{
+        keywords
+    }
+})
+
+// 获取热门搜索
+export const getHotSearch = () => request({
+    url:'/search/hot'
+})
